f6bd6cf8dabc4f0bc9c18837b669a493e127f355
Ethan Louis <ethanlouis768@gmail.com>
2024-01-09 12:06:38 -0600

    Add project files.

---
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SeniorProjectHealthApplication.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SeniorProjectHealthApplication.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
