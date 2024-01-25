07730f47c861c1cb4a3e4732ff365b5183657c88
Ethan Louis <ethanlouis768@gmail.com>
2024-01-25 15:16:15 -0600

    users database

---
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/Models/Database Structure/Users.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace SeniorProjectHealthApplication.Models.Database_Structure
+{
+    public class Users
+    {
+        [PrimaryKey, AutoIncrement]
+        public int UID { get; set; }
+        public string Username { get; set; }
+        
+    }
+}
