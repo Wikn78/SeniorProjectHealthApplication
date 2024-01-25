02ee40db1f2e815aa8a80fc4ec3d6bab096cbe9d
Ethan Louis <ethanlouis768@gmail.com>
2024-01-25 15:52:48 -0600

    Added lastname to Users.cs

--- SeniorProjectHealthApplication/SeniorProjectHealthApplication/Models/Database Structure/Users.cs
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/Models/Database Structure/Users.cs
@@ -9,7 +9,8 @@ namespace SeniorProjectHealthApplication.Models.Database_Structure
         public int UID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string FirstName { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
         public string Birthday { get; set; }
