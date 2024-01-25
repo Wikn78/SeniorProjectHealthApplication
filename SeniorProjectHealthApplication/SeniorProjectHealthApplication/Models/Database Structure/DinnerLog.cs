ae8effbacf0b74608bb1c101e7a8e9409084ef83
olek0019 <olek0019@UMC-1040-8134>
2024-01-25 15:49:40 -0600

    Capitalized data points in tables

--- SeniorProjectHealthApplication/SeniorProjectHealthApplication/Models/Database Structure/DinnerLog.cs
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/Models/Database Structure/DinnerLog.cs
@@ -6,9 +6,9 @@ namespace SeniorProjectHealthApplication.Models.Database_Structure
 {
     public class DinnerLog
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public int FL_ID { get; set; }
-        public string food_name { get; set; }
-        public float quantity { get; set; }
+        public string Food_Name { get; set; }
+        public float Quantity { get; set; }
     }
 }
--- SeniorProjectHealthApplication/SeniorProjectHealthApplication/Models/Database Structure/ExerciseLog.cs
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/Models/Database Structure/ExerciseLog.cs
@@ -6,10 +6,10 @@ namespace SeniorProjectHealthApplication.Models.Database_Structure
 {
     public class ExerciseLog
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public int UID { get; set; }
-        public string date { get; set; }
-        public string exercise { get; set; }
-        public float caloriesBurned { get; set; }
+        public string Date { get; set; }
+        public string Exercise { get; set; }
+        public float CaloriesBurned { get; set; }
     }
 }
