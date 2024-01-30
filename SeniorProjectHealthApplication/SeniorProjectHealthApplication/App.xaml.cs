790f725b682e38849c13ef403e451ca80b0c9e40
olek0019 <olek0019@crk.umn.edu>
2024-01-30 16:13:30 -0600

    Made basic layout of Notifications Page

--- SeniorProjectHealthApplication/SeniorProjectHealthApplication/App.xaml.cs
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/App.xaml.cs
@@ -14,7 +14,7 @@ namespace SeniorProjectHealthApplication
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            MainPage = new NotificationsPage();
         }
 
         protected override void OnStart()
