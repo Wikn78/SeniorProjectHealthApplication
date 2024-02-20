d17d6ab145101896dc55f02e298ea8037d64f366
olek0019 <olek0019@crk.umn.edu>
2024-02-20 15:54:20 -0600

    Created the layout for the forgot password page

--- SeniorProjectHealthApplication/SeniorProjectHealthApplication/App.xaml.cs
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/App.xaml.cs
@@ -14,7 +14,7 @@ namespace SeniorProjectHealthApplication
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            MainPage = new ForgotPasswordPage();
         }
 
         protected override void OnStart()
