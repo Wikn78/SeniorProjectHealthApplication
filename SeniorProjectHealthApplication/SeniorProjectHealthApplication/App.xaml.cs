d1add4bc32ec132a354998fb1dca0c1dad0f13a8
Ethan Louis <ethanlouis768@gmail.com>
2024-02-24 12:15:31 -0600

    Update UI color references to use resource dictionary
    Color references in the UI of the Health Application have been replaced with entries from the resource dictionary. This change accommodates the two newly created color resources "UMCMaroon" and "UMCGold". It also includes updates in XAML files and Android's Resource.designer.cs file. This change allows better code reusability and easier color management in future design updates.

--- SeniorProjectHealthApplication/SeniorProjectHealthApplication/App.xaml.cs
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/App.xaml.cs
@@ -14,7 +14,7 @@ namespace SeniorProjectHealthApplication
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new NavigationPage(new WorkoutScreen());
+            MainPage = new NavigationPage(new DashboardPage());
         }
 
         protected override void OnStart()
