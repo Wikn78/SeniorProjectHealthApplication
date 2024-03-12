950309c2b723e80f66fa44fd1958d891eded509f
olek0019 <olek0019@crk.umn.edu>
2024-03-12 12:21:21 -0500

    Added Fields to RecipesList Class

--- SeniorProjectHealthApplication/SeniorProjectHealthApplication/Models/Database Structure/RecipesList.cs
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/Models/Database Structure/RecipesList.cs
@@ -7,5 +7,16 @@ namespace SeniorProjectHealthApplication.Models.Database_Structure
         [PrimaryKey] [AutoIncrement] public int Id { get; set; }
 
         public string ReceipeName { get; set; }
+        public string RecipePicture { get; set; }
+        public double Calories { get; set; }
+        public double Carbs { get; set; }
+        public double Protein { get; set; }
+        public double Fat { get; set; }
+        public double SaturatedFat { get; set; }
+        public double Sodium { get; set; }
+        public double Fiber { get; set; }
+        public double Sugar { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
     }
 }
