316077bdcf42b026fce2baca61a45b4120bbe189
Maddi2001 <schn1757@crk.umn.edu>
2024-04-11 16:12:19 -0500

    hhgh

---
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/ViewModels/OpenWorkoutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SeniorProjectHealthApplication.ViewModels
+{
+    public class OpenWorkoutViewModel 
+    {
+        public ICommand OpenWorkoutCommand { get; set; }
+
+        public OpenWorkoutViewModel() 
+        {
+            OpenWorkoutCommand = new Command<string>(ExecuteOpenWorkout);  
+        }
+
+        private void ExecuteOpenWorkout(string workoutId)
+        {
+            int id = Int32.Parse(workoutId);
+
+
+        }
+
+    }
+}
--- SeniorProjectHealthApplication/SeniorProjectHealthApplication/Views/Workout/Bodyweight/Bodyweight_Intermediate.xaml.cs
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/Views/Workout/Bodyweight/Bodyweight_Intermediate.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SeniorProjectHealthApplication.Models.Database_Structure;
+using SeniorProjectHealthApplication.Models.DB_Repositorys;
+using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,14 +10,49 @@ namespace SeniorProjectHealthApplication.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WorkoutScreenPage : ContentPage
     {
+        private readonly DatabaseManager<ExerciseDatabase> _dbExerciseDatabase;
+
         public WorkoutScreenPage()
         {
             InitializeComponent();
+            _dbExerciseDatabase = LoadDatabase<ExerciseDatabase>();
         }
 
-        private async void Superman_Clicked(object sender, EventArgs e)
+        private async void Workout_Clicked(int workout_id, object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new LinktoVideosPage());
+
+            var webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                 
+
+                    Url = _dbExerciseDatabase.GetItem(10).Exercise_Link
+                   
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    webView
+                }
+            };
         }
+
+        private DatabaseManager<T> LoadDatabase<T>() where T : new()
+        {
+            string fileName = "ExerciseDatabase.db3";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string dbPath = Path.Combine(folderPath, fileName);
+
+
+
+            return new DatabaseManager<T>(dbPath);
+        }
+
     }
 }
