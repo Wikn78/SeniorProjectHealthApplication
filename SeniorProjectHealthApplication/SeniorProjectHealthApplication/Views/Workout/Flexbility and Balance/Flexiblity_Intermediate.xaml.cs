adc40ebd3184ef068cc6bc0d6e227f986825a17d
Maddi2001 <schn1757@crk.umn.edu>
2024-04-14 23:38:31 -0500

    Progress

---
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/Views/Workout/Flexbility and Balance/Flexiblity_Intermediate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SeniorProjectHealthApplication.Views.Workout.Flexbility_and_Balance
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Flexiblity_Intermediate : ContentPage
+    {
+        public Flexiblity_Intermediate()
+        {
+            InitializeComponent();
+        }
+    }
+}
