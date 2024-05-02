adb6fd011d511fba0dafb7c4371dc6112bdd6a42
ethanlouis <ethanlouis768@gmail.com>
2024-05-01 19:47:59 -0500

    Added tutorial button to WelcomePage
    Added a "View Tutorial" button to the WelcomePage. This button opens a WebView which displays a tutorial from a Google Drive link. A 'Back' button has also been added to close the WebView.

--- SeniorProjectHealthApplication/SeniorProjectHealthApplication/Views/Account/WelcomePage.xaml.cs
+++ SeniorProjectHealthApplication/SeniorProjectHealthApplication/Views/Account/WelcomePage.xaml.cs
@@ -60,7 +60,8 @@ namespace SeniorProjectHealthApplication.Views
             });
 
 
-            await Navigation.PushAsync(new GoalsStartPage(CurrentWeight.SelectedItem.ToString(), GoalWeight.SelectedItem.ToString()));
+            await Navigation.PushAsync(new GoalsStartPage(CurrentWeight.SelectedItem.ToString(),
+                GoalWeight.SelectedItem.ToString()));
         }
 
 
@@ -71,6 +72,32 @@ namespace SeniorProjectHealthApplication.Views
             var dbPath = Path.Combine(folderPath, fileName);
 
             return new DatabaseManager<UserAppInfo>(dbPath);
+        }
+
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                    Url = "https://drive.google.com/file/d/1I3urWoXcyO1c3PA_8giSROMMkcsq5wHz/view?usp=sharing"
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var contentPage = new ContentPage { Content = webView };
+
+            // Add a 'Back' Button to close the WebView
+            contentPage.ToolbarItems.Add(new ToolbarItem
+            {
+                Text = "Back",
+                Command = new Command(async () => await Application.Current.MainPage.Navigation.PopModalAsync())
+            });
+
+            // Open WebView as a Modal
+            await Application.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(contentPage));
         }
     }
 }
